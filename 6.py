2c2202528bb59a8497e6f13e0854e231c65ef940
squeh <itsreallysqueh@gmail.com>
2025-12-17 11:47:46 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,9 +2,4 @@ word = input("Введите слово: ")
 listt = list(word)
 reversed_list = listt[::-1]
 
-if listt == reversed_list:
-    print("Это палиндром")
-else:
-    print("Не палиндром")
-
 print(f"{listt} == {reversed_list}")
